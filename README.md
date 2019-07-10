9a6a8a16df5daf87bc6ffa3afbe9723e41996462
Glenn Lopez <glennlopez@gmail.com>
2019-07-10 09:41:52 -0500

    tutorial checkpoint

--- README.md
+++ README.md
@@ -5,20 +5,24 @@
 <br/>
 
 <!-- <div align="center"><img src="demo.gif"></img></div> -->
-<h2>About</h2>
-This repository is to act as proof-of-understanding to prospective employers. It allows you (as an employer/hiring manager) to interact with my work rather than banking on the marks and test scores I recieved in school.
+<h2>About:</h2>
+This repository acts as proof-of-understanding to prospective employers. It allows you (as an employer/hiring manager) to interact with the concepts I learned, tutorials I used, and mistakes plus corrections I made, rather than banking on the marks and test scores I recieved in school to make your decision.
 
-<h2>Goal and requirements</h2>
-Familiarise myself with the basics of WPF and MVVM model, allowing me to make my own software/utilities for my day-to-day life, and work related tasks.
+<h2>What I learned:</h2>
 
-<h2>Key learnings</h2>
+- <b>[UI Layout using XAML](https://github.com/glennlopez/WPFUI.LearningProject/tree/master/WpfBasics)</b>
+  - Basic XML
+  - XAML Template Structure
+- <b>TreeViews and Value Converters</b>
+  - [Basic Data Binding](https://youtu.be/6OwyNiLPDNw?t=1032)
+  - Styling Properties [1]
 
-- UI Layout using XAML
+Notes I took for myself are also provided in the Wiki page of this repository. They are added on the fly and may or may not be "user-friendly".
 
+<h2>What I did with what I learned:</h2>
 
-<h2>Credits</h2>
+- Work in progress...
+
+<h2>Credits:</h2>
 
 - YouTuber: <a href="https://www.youtube.com/channel/UCJ3AxeCHGPZkMi3kRfCuiHw" target="_blank">AngelSix</a> for Tutorial Content
-
-<h2>Copyright</h2>
-This project is licensed under the terms of the MIT license and protected by Udacity Honor Code and Community Code of Conduct. See <a href="LICENSE.md">license</a> and <a href="LICENSE.DISCLAIMER.md">disclaimer</a>.
---
+++ WpfTreeView/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTreeView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Get every logical drive
+            foreach (var drive in Directory.GetLogicalDrives())
+            {
+                // create new item for every logical drive
+                var item = new TreeViewItem()
+                {
+                    // {set} header
+                    Header = drive,
+
+                    // {set} full path
+                    Tag = drive
+                };
+               
+
+                // add sub item to item (dummy item)
+                item.Items.Add(null);
+
+                // listen for expanded item
+                item.Expanded += Folder_Expanded;
+
+                // add item to FolderView (TreeViewItem)  
+                FolderView.Items.Add(item);
+            }
+        }
+
+
+        private void Folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+
+            // do this if item is not a dummy item
+            if (item.Items.Count != 1 || item.Items[0] != null)
+            {
+                return;
+            }
+
+            // remove dummy data
+            item.Items.Clear();
+
+            // get fullpath name
+            var fullpath = (string)item.Tag;
+
+        }
+    }
+}
